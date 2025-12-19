ac18f0a7e042de7124f27dacf56f91a54ea5d474
deberkles125-sys <deberkles125@gmail.com>
2025-12-19 18:32:20 +0100

    added a section to the server-statistics.sh file

--- server-statistics.sh
+++ server-statistics.sh
@@ -8,9 +8,14 @@ echo "CPU Usage:"
 ps -eo %cpu,%mem,cmd --sort=-%cpu | head -n 5
 echo "-----------------------------------"
 
+# Sorted by memory
+echo "Top 5 Memory Consumors:"
+ps -eo %mem,%cpu,cmd --sort=-%mem | head -n 5
+echo "-----------------------------------"  
+
 # Memory Usage
 echo "Memory Usage:"
-free -h | head -n 5
+free -h 
 echo "-----------------------------------"
 
 # Disk Usage
